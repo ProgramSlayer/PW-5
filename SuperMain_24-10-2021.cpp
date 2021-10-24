8f1226e4686203347e3946927d5b4d2d24f29555
ProgramSlayer <80097802+ProgramSlayer@users.noreply.github.com>
2021-10-24 14:11:03 +0300

    Файлы проекта
    Sample text

---
+++ SuperMain_24-10-2021.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <ctime>
+#include <string>
+#include <locale>
+#include <cstdlib>
+#include "Matrix.h"
+#include "FileReader.h"
+#include "Decomposition.h"
+#include "Solver.h"
+#include "Timer.h"
+
+// Примитивный вывод элементов матрицы на консоль.
+void printMatrix(const Matrix& m, const unsigned int precision = 6)
+{
+	if (m.get_cSize() == 0 || m.get_rSize() == 0)
+	{
+		std::cout << "Matrix is zero!\n\n";
+		return;
+	}
+
+	for (size_t i{ 0 }; i != m.get_rSize(); ++i)
+	{
+		for (size_t j{ 0 }; j != m.get_cSize(); ++j)
+		{
+			//std::cout.width(20);
+			std::cout << std::setprecision(precision) << std::scientific << m(i, j) << '\t';
+		}
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+}
+
+/// <summary>
+/// Возвращает (псевдо)случайное число в заданном диапазоне.
+/// </summary>
+/// <param name="min"> - нижняя граница диапазона.</param>
+/// <param name="max"> - верхняя граница диапазона.</param>
+/// <returns>(Псевдо)случайное число в заданном диапазоне.</returns>
+double getRandomRealNumber(const double min = -100, const double max = 100)
+{
+	// Генератор, который инициализируется от времени вызова функции time().
+	static std::mt19937 generator{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+	// Размещение, которое как раз и будет возвращать случайное число.
+	std::uniform_real_distribution<> d{ min, max };
+	return d(generator);
+}
+
+/// <summary>
+/// Заполняет переданную матрицу (псевдо)случайными числами в заданном диапазоне.
+/// </summary>
+/// <param name="m"> - матрица для заполнения.</param>
+/// <param name="min"> - нижняя граница диапазона.</param>
+/// <param name="max"> - верхняя граница диапазона.</param>
+void fillMatrixWithRandomNumbers(Matrix& m, const double min = -100, const double max = 100)
+{
+	for (size_t i{}; i != m.get_rSize(); ++i)
+		for (size_t j{}; j != m.get_cSize(); ++j)
+			m(i, j) = getRandomRealNumber(min, max);
+}
+
+// Пользовательский ввод наименования файла
+std::string userFileInput(const std::string& filePath)
+{
+	std::string input{};
+	std::ifstream tryToOpenFile;
+	while (true)
+	{
+		std::cout << "Введите имя и расширение файла.\n";
+		std::cout << "Ваш ввод >> ";
+		std::getline(std::cin, input);
+		if (!input.find(".txt"))
+		{
+			std::cout << "Лее, файл без расширения!\n\n";
+			continue;
+		}
+		else
+		{
+			tryToOpenFile.open(std::string(filePath + input));
+			if (!tryToOpenFile.is_open())
+			{
+				std::cout << "Лее, файл не найден!\n\n";
+				continue;
+			}
+		}
+		return input;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp1251");
+	system("cls");
+	while (true) {
+
+		std::string path{ ".\\Matrixes\\" };
+
+		// Полный путь к файлу
+		std::string fullPathAndName{ path + userFileInput(path) }; 
+
+		try{ 
+			Matrix a{ read(fullPathAndName) }; 
+
+			// Проверка определителя матрицы после считывания из файла
+			if (a.det() == 0.0) {
+				throw "Error: determinant = 0\n" ;
+			}
+
+			// Точное решение СЛАУ.
+			Matrix x_exist{ a.get_rSize(), 1 };
+			fillMatrixWithRandomNumbers(x_exist);
+
+			// Вектор правой части.
+			Matrix b{ a * x_exist };
+
+			Timer t;
+
+			// Решение СЛАУ методом Крамера.
+			Matrix x_Cramer{ Solver::solveSoLAE(a, b) };
+			auto cramerMethodTimeElapsed{ t.elapsed() };
+
+			t.reset();
+
+			// Решение СЛАУ через LU-разложение.
+			Matrix x_LU{ Solver::solveSoLAE(a, b, Solver::SolutionMethod::LU_DECOMPOSITION) };
+
+			auto luMethodTimeElapsed{ t.elapsed() };
+
+			std::cout << "Cramer method time elapsed: " << std::setprecision(15) << std::scientific << cramerMethodTimeElapsed << '\n';
+			std::cout << "LU method time elapsed: " << std::setprecision(15) << std::scientific << luMethodTimeElapsed << "\n\n";
+
+			std::cout << "|| x_exist - x_Cramer || = " << std::setprecision(15) << std::scientific << (x_exist - x_Cramer).norm() << '\n';
+			std::cout << "|| x_exist - x_LU ||     = " << std::setprecision(15) << std::scientific << (x_exist - x_LU).norm() << '\n';
+			std::cout << "|| x_Cramer - x_LU ||    = " << std::setprecision(15) << std::scientific << (x_Cramer - x_LU).norm() << '\n';
+			break;
+		}
+		catch(const char* e) {
+			std::cout<< e << '\n';
+		}
+		catch (...) {
+			std::cout << "Что-то не так!"<< '\n';
+		}
+	}
+			
+	return 0;
+}
---
+++ Timer.h
@@ -0,0 +1,31 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <chrono> // for std::chrono functions
+
+class Timer
+{
+private:
+	// Type aliases to make accessing nested type easier.
+	using clock_type = std::chrono::steady_clock;
+	using second_type = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<clock_type> m_beg;
+
+public:
+	Timer() : m_beg{ clock_type::now() }
+	{
+	}
+
+	void reset()
+	{
+		m_beg = clock_type::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast<second_type>(clock_type::now() - m_beg).count();
+	}
+};
+
+#endif // !TIMER_H
